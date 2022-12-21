51a66ef3c5e24f4ec413c1d2c57bb8829ad81832
solaris5000 <solaris5000tv@gmail.com>
2022-12-21 12:22:32 +0600

    added some code that print some shit to console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let some_string : &str = "This is a some test string for \n";
+    print!("{}a user soalris5000\n", some_string);
+
+    let mut loop_x : i32 = 0;
+    loop {
+        if loop_x > 10 {break;}
+        print!("{}\n",loop_x);
+        loop_x +=1;
+    }
 }
