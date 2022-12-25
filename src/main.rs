b17cef7301e9818647bac20d29f3502261d1f910
solaris5000 <solaris5000tv@gmail.com>
2022-12-25 11:45:59 +0600

    Added some functions placeholder, need to complete them later

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
 use std::io;
+
+//function to get a next number
+fn getNumber() -> f32 {
+    0.0
+}
+
+//function to get a next char to parse it in operation
+fn getChar() -> char {
+    '+'
+}
+
+// well, that func printing 5 last operations, if there was 5 operations
+fn printHistory(HistoryArray :&[(bool,f32, char, f32, f32)]) {
+    print!("There is no history for now ;)\n")
+}
+
+//fucn to do all the math in this prog.
+fn doMath (var_a :&f32, var_b :&f32, sym :&char) -> f32 {
+    return 0.0;
+}
  
 fn main() -> io::Result<()> {
 
     let mut f_var_a : f32 = 0.0;
     let mut f_var_b : f32 = 0.0;
-
     let mut f_result : f32 = 0.0;
-
     let mut choise : char = '+';
 
+    // This array will be used to store history of operations.
+    let mut a_history_of_t: [(bool,f32, char, f32, f32); 5] = [
+        (false, 0.0, '0', 0.0, 0.0),
+        (false, 0.0, '0', 0.0, 0.0),
+        (false, 0.0, '0', 0.0, 0.0),
+        (false, 0.0, '0', 0.0, 0.0),
+        (false, 0.0, '0', 0.0, 0.0)];
 
+/* 
+    loop{
+        f_var_a = getNumber();
+        choise = getChar();
+        f_var_b = getNumber();
+        f_result = doMath(&f_var_a, &f_var_b, &choise);
+    }
+*/
     let mut input = String::new();
 
     println!("Enter 1 var: ");
